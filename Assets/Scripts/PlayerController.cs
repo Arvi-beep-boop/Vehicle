6075fa1d2190b2cbec88b5bca651446bfd8a79a6
Arvi-beep-boop <xarvichanx@gmail.com>
2023-05-02 22:12:42 +0200

    Vehicle and Camera Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 15.0f;
+    private float turnSpeed = 40.0f;
+    private float horizontalInput;
+    private float verticalInput;
     void Start()
     {
         
@@ -11,7 +15,14 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // move the vehicle   
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // Player Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Moves the vehicle forward based on vertical input  
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, horizontalInput *turnSpeed * Time.deltaTime);
     }
 }
