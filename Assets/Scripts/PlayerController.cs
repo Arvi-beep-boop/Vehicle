cac7d7ca4e658b186f885f5a4c240113d31dd41b
Arvi-beep-boop <xarvichanx@gmail.com>
2023-06-11 15:15:14 +0200

    Camera Toggle & Local Multiplayer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public string inputID;
+    public KeyCode cameraToggleKey;
+    public Camera mainCamera;
+    public Camera hoodCamera;
     private float speed = 15.0f;
     private float turnSpeed = 40.0f;
     private float horizontalInput;
@@ -16,13 +20,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Player Input
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        verticalInput = Input.GetAxis("Vertical" + inputID);
 
         // Moves the vehicle forward based on vertical input  
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
 
         // Rotates the vehicle based on horizontal input
         transform.Rotate(Vector3.up, horizontalInput *turnSpeed * Time.deltaTime);
+
+        if(Input.GetKeyDown(cameraToggleKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
